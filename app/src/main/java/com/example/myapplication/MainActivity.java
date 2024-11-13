3eef90fadf8af56030e538013287e1fa340400ea
ahmaddiab <dybhmd83@gmail.com>
2024-11-13 15:18:24 -0800

    ahmad

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 private DashFrag dashFrag;
 private HomeFrag homeFrag;
 private loginFrag loginFrag;
-private FrameLayout homeFrame,dashFrame,loginFrame;
+public static FrameLayout homeFrame,dashFrame,loginFrame;
 
 private BottomNavigationView bottomNav;
     @Override
@@ -27,9 +27,9 @@ private BottomNavigationView bottomNav;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        loginFrame = findViewById(R.id.login_frame);
         homeFrame = findViewById(R.id.home_frame);
         dashFrame = findViewById(R.id.dash_frame);
-        loginFrame = findViewById(R.id.login_frame);
         bottomNav = findViewById(R.id.bottom_navigation);
         begin();
 
@@ -38,26 +38,29 @@ private BottomNavigationView bottomNav;
 
     }
     private void begin(){
+        loginFrag=new loginFrag();
         homeFrag=new HomeFrag();
         dashFrag=new DashFrag();
-        loginFrag=new loginFrag();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.login_frame,loginFrag).commit();
         getSupportFragmentManager().beginTransaction().replace(R.id.home_frame,homeFrag).commit();
         getSupportFragmentManager().beginTransaction().replace(R.id.dash_frame,dashFrag).commit();
-        getSupportFragmentManager().beginTransaction().replace(R.id.login_frame,loginFrag).commit();
+
         dashFrame.setVisibility(View.INVISIBLE);
-        loginFrame.setVisibility(View.INVISIBLE);
+        homeFrame.setVisibility(View.INVISIBLE);
+        loginFrame.setVisibility(View.VISIBLE);
         bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId()==R.id.menu_home){
-                    homeFrame.setVisibility(View.VISIBLE);
+                if(item.getItemId()==R.id.menu_login){
+                    homeFrame.setVisibility(View.INVISIBLE);
                     dashFrame.setVisibility(View.INVISIBLE);
-                    loginFrame.setVisibility(View.INVISIBLE);
+                    loginFrame.setVisibility(View.VISIBLE);
 
 
                 }
 
-                if(item.getItemId()==R.id.menu_home){
+                if(item.getItemId()==R.id.menu_dashboard){
                     homeFrame.setVisibility(View.INVISIBLE);
                     dashFrame.setVisibility(View.VISIBLE);
                     loginFrame.setVisibility(View.INVISIBLE);
@@ -66,9 +69,9 @@ private BottomNavigationView bottomNav;
                 }
 
                 if(item.getItemId()==R.id.menu_home){
-                    homeFrame.setVisibility(View.INVISIBLE);
+                    homeFrame.setVisibility(View.VISIBLE);
                     dashFrame.setVisibility(View.INVISIBLE);
-                    loginFrame.setVisibility(View.VISIBLE);
+                    loginFrame.setVisibility(View.INVISIBLE);
 
 
                 }
--- app/src/main/java/com/example/myapplication/loginFrag.java
+++ app/src/main/java/com/example/myapplication/loginFrag.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,13 +11,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class loginFrag extends Fragment {
 
-TextInputEditText textUser,textPassword;
-Button login;
+private TextInputEditText textUser,textPassword;
+private Button btnlogin;
+private FirebaseAuth mAuth;
 
     public loginFrag() {
         // Required empty public constructor
@@ -27,21 +33,51 @@ Button login;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mAuth=FirebaseAuth.getInstance();
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         textUser = view.findViewById(R.id.etName);
         textPassword=view.findViewById(R.id.password);
-        login=view.findViewById(R.id.button);
-        login.setOnClickListener(new View.OnClickListener() {
+        btnlogin=view.findViewById(R.id.button);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user = textUser.getText().toString();
-                String pass = textPassword.getText().toString();
-                if(user.equals("kondara")&& pass.equals("1234")){
-                    Toast.makeText(getContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+checkEmailPassword();
                 }
-            }
+
         });
                 return view;
     }
+    private void checkEmailPassword() {
+        String email=textUser.getText().toString();
+        String password=textPassword.getText().toString();
+        if(!email.isEmpty() && !password.isEmpty()){
+           mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+               @Override
+               public void onComplete(@NonNull Task<AuthResult> task) {
+                   if(task.isSuccessful()){
+                       uptadeUi();
+
+                       Toast.makeText(getActivity(), "Login Successfull", Toast.LENGTH_SHORT).show();
+                   }
+                   else{
+                       Toast.makeText(getActivity(), "Login Failed", Toast.LENGTH_SHORT).show();
+
+                   }
+               }
+           });
+        }
+        else{
+            Toast.makeText(getActivity(), "Please enter email and password", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+}
+
+public void uptadeUi(){
+        MainActivity.loginFrame.setVisibility(View.INVISIBLE);
+        MainActivity.homeFrame.setVisibility(View.VISIBLE);
+        MainActivity.dashFrame.setVisibility(View.INVISIBLE);
+}
 }
